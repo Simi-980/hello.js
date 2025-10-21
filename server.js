813a54f2cdd9fd60b93dbe93124cf99f662d2663
Simi-980 <oyinlolasimisola21@gmail.com>
2025-10-21 18:35:47 +0100

    server.js

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ app.post('/', (req, res) => {
     console.log(req.body);
     res.json({ echoed: req.body });
 
+    app.post("/user", (req, res) => {
+        const { name, email } = req.body; 
+        if (name) return res.status(400).send("Name required!");
+        res.json({ message: 'Hello, $[name]! Your $[email]'});
+    });
 });
 
 app.get("/user/:id", (req, res) =>{
-    const id = req.params.id;
+    const userid = req.params.id;
     console.log(id);
-    res.send("User[id]profile");
+    res.send('User[id]profile: ${userid}');
+});
+
+app.get("/search", (req, res) => {
+    const userid = req.query.id;
+    console.log(id);
+    res.send(id);
 });
 
 app.get('/', (req, res) => {
