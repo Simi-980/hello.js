629d28efe4cad7bbe3a6280a5b51ea3e3a6f7185
Simi-980 <oyinlolasimisola21@gmail.com>
2025-10-21 18:53:53 +0100

    index.html

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
+
+//serve a static HTML page 
+   app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, 'static', 'index.html'))
+   });
 
 app.use(express.json()); 
 
