f66b6ca7e4e40f1bcbf300b2340ac6ba21d0943d
Simi-980 <oyinlolasimisola21@gmail.com>
2025-10-21 17:15:48 +0100

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Middleware
+
+app.post('/echo', (req, res) => { // Post Route
+    res.json({ echoed: req.body }); // Means that the req.body is nw available 
+});
 
 app.get('/', (req, res) => {
   res.send('My week 2 API!');
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
